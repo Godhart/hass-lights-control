0a82de2923671cec9e839e3b8fda980ed3af15ed
Николай Гнитеев <godhart@gmail.com>
2020-03-18 23:59:12 +0300

    tests: included simple template for pytest

---
+++ tests/test_lights_control.py
@@ -0,0 +1,15 @@
+import pytest
+
+def setup_module(module):
+    #init_something()
+    pass
+
+def teardown_module(module):
+    #teardown_something()
+    pass
+
+def test_pass():
+    assert True
+    
+def test_fail():
+    assert False
